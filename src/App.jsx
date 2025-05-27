e95c7b1a028faf8e9adcef26f45857680a079ed7
yuvraj singh sisodiya <uvrajput0107@gmail.com>
2025-05-28 01:33:51 +0530

    first commit project is comtited

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,1 @@
-import { useState } from 'react'
 
-
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className='bg-green-400 font-extralight '>hello world</div>
-    </>
-  )
-}
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,60 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contect-us/Contect.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      },
+      {
+        path: "contect",
+        element: <Contact />
+      }
+    ],
+    
+    
+
+  }
+  
+],
+    {
+      basename: "/react-router",
+    }
+
+)
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path='/' element={<Layout />}>
+  //       <Route path="" element ={<Home/>}/>
+  //       <Route path="about" element ={<About/>}/>
+  //       <Route path="contect" element ={<Contact/>}/>
+
+  //     </Route>
+  //     ,
+  //     {
+  //     basename: "/react-router",
+  //     }   
+  //   )
+  // )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
